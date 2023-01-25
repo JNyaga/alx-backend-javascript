a48f597a944bdff879d6fc310a206db0020f2db4
JNyaga <joelnyagamuriithi@gmail.com>
2023-01-26 02:38:13 +0300

    promises and async

--- 0x00-ES6_basic/tests/2-main.js
+++ 0x00-ES6_basic/tests/2-main.js
@@ -1,4 +1,5 @@
-import getNeighborhoodsList from './2-arrow.js';
+/* eslint-disable new-cap */
+import getNeighborhoodsList from '../2-arrow';
 
 const neighborhoodsList = new getNeighborhoodsList();
 const res = neighborhoodsList.addNeighborhood('Noe Valley');
