7e621d2eea15e3ff17c3509c43916b2411bd3ed7
JNyaga <joelnyagamuriithi@gmail.com>
2023-03-24 03:26:28 +0300

    calcul

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,21 +1,21 @@
 const assert = require('assert');
+const {it, describe} = require('mocha');
 const calculateNumber = require('./0-calcul');
 
-
-describe('calculateNumber()', () => {
-  it('should return 4 after adding 1 and 3', () => {
-    assert.equal(calculateNumber(1, 3), 4);
-  });
-  it('should return 5 after adding 1 and 3.7', () => {
-    assert.equal(calculateNumber(1, 3.7), 5);
-  });
-  it('should return 5 after adding 1.2 and 3.7', () => {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-  });
-  it('should return 6 after adding 1.5 and 3.7', () => {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-  });
-  it('should return -3 after adding -1.0 and -2', () => {
-    assert.equal(calculateNumber(-1.0, -2), -3);
-  });
+describe('calculateNumber', () => {
+    it('checking if numbers round', () => {
+        assert.equal(calculateNumber(1, 3), 4);
+    });
+    it('checking if numbers round 2nd round', () => {
+        assert.equal(calculateNumber(1, 3.7), 5);
+    });
+    it('checking if numbers round 3nd round', () => {
+        assert.equal(calculateNumber(1.2, 3.7), 5);
+    });
+    it('checking if numbers round 4nd round', () => {
+        assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
+    it('checking negative return', () => {
+        assert.equal(calculateNumber(-1.3, -3.7), -5);
+      });
 });
